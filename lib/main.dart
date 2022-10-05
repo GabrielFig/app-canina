2fe7931abfa7ae3463ecbbe31c88a7c7538f07db
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-10-04 22:29:03 -0600

    add aplication with bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:dog_app/bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const Aplicacion());
+}
+
+class AplicacionInyectada extends StatelessWidget {
+  const AplicacionInyectada({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => BlocVerificacion(),
+      child: const Aplicacion(),
+    );
+  }
 }
 
 class Aplicacion extends StatelessWidget {
