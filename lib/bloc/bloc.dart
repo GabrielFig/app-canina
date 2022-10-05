c9ab9e96dd422ac7474e6f40f80410aff25bea03
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-10-04 21:52:21 -0600

    Add more states

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -14,6 +14,14 @@ class Creandose extends Estado {}
 
 class SolicitandoRaza extends Estado {}
 
+class EsperandoConfirmacionRaza extends Estado {}
+
+class MostrandoSolicitudActualizacion extends Estado {}
+
+class MostrandoRazaNoConfirmada extends Estado {}
+
+class MostrandoRazaConfirmada extends Estado {}
+
 class BlocVerificacion extends Bloc<Evento, Estado> {
   BlocVerificacion() : super(Creandose()) {
     on<Creado>((event, emit) {
