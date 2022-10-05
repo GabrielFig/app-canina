60ecdd3defc242eac5ce9e9d20665251bb0332d4
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-10-04 23:02:08 -0600

    fix error with run app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const Aplicacion());
+  runApp(const AplicacionInyectada());
 }
 
 class AplicacionInyectada extends StatelessWidget {
