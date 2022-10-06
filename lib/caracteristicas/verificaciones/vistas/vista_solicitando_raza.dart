c16fc9e605bc5beb2ccdd8e437309b88f0bf40d7
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-10-06 00:42:58 -0600

    improvements in design of get breed name

--- lib/caracteristicas/verificaciones/vistas/vista_solicitando_raza.dart
+++ lib/caracteristicas/verificaciones/vistas/vista_solicitando_raza.dart
@@ -1,16 +1,91 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:dog_app/bloc/bloc.dart';
+import 'package:dog_app/dominio/variable_dominio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class VistaSolicitandoRaza extends StatelessWidget {
+class VistaSolicitandoRaza extends StatefulWidget {
   const VistaSolicitandoRaza({Key? key}) : super(key: key);
 
   @override
+  State<VistaSolicitandoRaza> createState() => _VistaSolicitandoRazaState();
+}
+
+class _VistaSolicitandoRazaState extends State<VistaSolicitandoRaza> {
+  bool _nombreRazaValidado = false;
+  late final TextEditingController controlador;
+
+  @override
+  void initState() {
+    controlador = TextEditingController();
+    controlador.addListener(escuchadorValidador);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controlador.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text("Dame una raza"),
-        const TextField(),
-        TextButton(onPressed: () {}, child: const Text("algo"))
-      ],
+    final elBloc = context.read<BlocVerificacion>();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter',
+            ),
+            controller: controlador,
+          ),
+          Container(
+            child: _nombreRazaValidado
+                ? null
+                : TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.grey),
+                    ),
+                    onPressed: null,
+                    child: const Text('Buscar Raza'),
+                  ),
+          ),
+          Container(
+            child: !_nombreRazaValidado
+                ? null
+                : TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      elBloc.add(
+                        NombreRazaRecibido(
+                          NickFormado.constructor(controlador.text),
+                        ),
+                      );
+                    },
+                    child: const Text('Buscar Raza'),
+                  ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void escuchadorValidador() {
+    setState(() {
+      try {
+        NickFormado.constructor(controlador.text);
+        _nombreRazaValidado = true;
+      } catch (e) {
+        _nombreRazaValidado = false;
+      }
+    });
   }
 }
