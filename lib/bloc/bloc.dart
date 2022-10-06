4dced64c3cf57353c2963cf455615e93bb7cc8b6
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-10-06 00:40:37 -0600

    add new events and improvements in bloc verification

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,8 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:dog_app/caracteristicas/repositorio_verificacion.dart';
+import 'package:dog_app/dominio/problemas.dart';
+import 'package:dog_app/dominio/registro_raza.dart';
+import 'package:dog_app/dominio/variable_dominio.dart';
 
 class Evento {}
 
 class Creado extends Evento {}
+
+class NombreRazaRecibido extends Evento {
+  final NickFormado nick;
+  NombreRazaRecibido(this.nick);
+}
+
+class MostrandoRazaConfirmada extends Estado {
+  final RegistroRaza registroRaza;
+  final NickFormado nombreRaza;
+
+  MostrandoRazaConfirmada(this.registroRaza, this.nombreRaza);
+}
+
+class MostrandoRazaNoConfirmada extends Estado {
+  final NickFormado nombreRazaNoCofirmada;
+
+  MostrandoRazaNoConfirmada(this.nombreRazaNoCofirmada);
+}
+
+class MostrandoRazaSinSubRazas extends Estado {
+  final NickFormado nombreRazaSinSubRazas;
+
+  MostrandoRazaSinSubRazas(this.nombreRazaSinSubRazas);
+}
 
 class RazaRecibida extends Evento {}
 
@@ -18,14 +46,25 @@ class EsperandoConfirmacionRaza extends Estado {}
 
 class MostrandoSolicitudActualizacion extends Estado {}
 
-class MostrandoRazaNoConfirmada extends Estado {}
-
-class MostrandoRazaConfirmada extends Estado {}
-
 class BlocVerificacion extends Bloc<Evento, Estado> {
-  BlocVerificacion() : super(Creandose()) {
+  final repositoriVerificacion _repositorioVerificacion;
+  BlocVerificacion(this._repositorioVerificacion) : super(Creandose()) {
     on<Creado>((event, emit) {
       emit(SolicitandoRaza());
+    });
+
+    on<NombreRazaRecibido>((event, emit) {
+      emit(EsperandoConfirmacionRaza());
+      final resultado =
+          _repositorioVerificacion.obtenerRegistroRaza(event.nick);
+      resultado.match((l) {
+        if (l is VersionIncorrectaJson) emit(MostrandoSolicitudActualizacion());
+        if (l is RazaNoEncontrada) emit(MostrandoRazaNoConfirmada(event.nick));
+        if (l is SubRazasNoEncontradas)
+          emit(MostrandoRazaSinSubRazas(event.nick));
+      }, (r) {
+        emit(MostrandoRazaConfirmada(r, event.nick));
+      });
     });
   }
 }
